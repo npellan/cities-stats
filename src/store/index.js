cf51311461a688918b91f1801d40e5c63a4bedc1
Nicolas <npellan@gmail.com>
2021-04-01 10:32:31 +0200

    ♻️  added getters on the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,32 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import cities from '../assets/data/data.json';
 
 Vue.use(Vuex);
 
+const getters = {
+  getCityById: (state) => (cityId) => state.cities.find((city) => city.id === cityId),
+};
+
+const mutations = {
+  SELECT_CITY: (state, city) => {
+    const cityId = city.slice(4);
+    state.selectedCity = cityId;
+  },
+};
+
+const actions = {};
+
+const state = {
+  cities,
+  selectedCity: '',
+};
+
 const store = new Vuex.Store({
-  state: {
-    stats: [],
-    selectedCity: '',
-  },
-  mutations: {
-    selectCity(state, city) {
-      state.selectedCity = city;
-    },
-  },
+  state,
+  getters,
+  mutations,
+  actions,
 });
 
 export default store;
