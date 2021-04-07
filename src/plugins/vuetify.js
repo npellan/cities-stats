0c194d11ccfb238e6d5117267a294ed4a63fb28f
Nicolas <npellan@gmail.com>
2021-04-07 17:36:43 +0200

    💄  vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      dark: {
+        background: '#ebebe3',
+        color: '#4e4e4d',
+      },
+    },
+    dark: true,
+  },
 });
