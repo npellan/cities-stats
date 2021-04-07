82abcf223895e5e6dc33d1bfc82365a97a8a3078
Nicolas <npellan@gmail.com>
2021-04-07 17:35:36 +0200

    🏗  rearranged components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
   <v-app>
-    <div id="app">
-        <CitiesStats />
+    <Header />
+
+    <div class="container">
+      <Map />
+      <Stats />
     </div>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import CitiesStats from './components/CitiesStats.vue';
+import Header from './components/Header.vue';
+import Map from './components/Map.vue';
+import Stats from './components/Stats.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    CitiesStats,
+    Header,
+    Map,
+    Stats,
+    Footer,
   },
 };
 </script>
 
 <style lang="scss">
-  #app {
+  body {
+    background-color: #ebebe3;
+    color: #4e4e4d
+  }
+
+  .v-application {
     width: 80%;
     max-width: 1440px;
     margin: 0 auto;
+    background-color: var(--v-background-base) !important;
+    color: var(--v-color-base) !important;
+  }
+
+  .container {
+    display: flex;
   }
 </style>
